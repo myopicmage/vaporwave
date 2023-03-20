9508a7d8dfe5964cc09291ce34736775cbbfa268
Kevin Bernfeld <kcbernfeld@gmail.com>
2023-03-20 13:34:21 -0400

    deploy maybe

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,4 +1,5 @@
 import Fluent
+import FluentPostgresDriver
 import FluentSQLiteDriver
 import Leaf
 import Vapor
@@ -8,7 +9,14 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    if app.environment == .production {
+        if let databaseURL = Environment.get("DATABASE_URL") {
+            try app.databases.use(.postgres(url: databaseURL), as: .psql)
+        }
+    } else {
+        app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    }
+
 
     app.migrations.add(CreateCategory())
     app.migrations.add(CreateTodo())
